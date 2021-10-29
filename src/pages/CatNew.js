05e24dbc20671444f5176972d9d1c50a5c15ce34
sjproctor <sjproctor950@gmail.com>
2021-10-29 11:55:51 -0700

    adds form and listener event

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
 class CatNew extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      form: {
+        name: "",
+        age: "",
+        enjoys: ""
+      }
+    }
+  }
+
+  handleChange = (e) => {
+    let { form } = this.state
+    form[e.target.name] = e.target.value
+    this.setState({form: form})
+  }
+
   render() {
+    console.log(this.state.form)
     return (
       <>
         <h3>I am the Cat New</h3>
+        <Form>
+          <FormGroup>
+            <Label for="name">
+              Cat's Name
+            </Label>
+            <Input
+              name="name"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.form.name}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="age">
+              Cat's Age
+            </Label>
+            <Input
+              name="age"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.form.age}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="enjoys">
+              Cat's Hobbies and Enjoyment
+            </Label>
+            <Input
+              name="enjoys"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.form.enjoys}
+            />
+          </FormGroup>
+          <Button>
+            Add a New Cat
+          </Button>
+        </Form>
       </>
     )
   }
